1fe6e6466c7ab8bf7d2922b63d8e5a08c41bae12
falcondive <falstoop@gmail.com>
2020-07-26 23:54:19 +0530

    session timeout

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,20 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
-from flask import Flask, url_for
+from flask import Flask, url_for,session
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from importlib import import_module
 from logging import basicConfig, DEBUG, getLogger, StreamHandler
 from os import path
+from datetime import timedelta
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.refresh_view = 'relogin'
+login_manager.needs_refresh_message = (u"Session timedout, please re-login")
+login_manager.needs_refresh_message_category = "info"
 
 def register_extensions(app):
     db.init_app(app)
@@ -39,4 +44,12 @@ def create_app(config):
     register_extensions(app)
     register_blueprints(app)
     configure_database(app)
+    configure_session(app)
     return app
+
+def configure_session(app):
+
+    @app.before_request
+    def before_request():
+        session.permanent = True
+        app.permanent_session_lifetime = timedelta(minutes=60)
